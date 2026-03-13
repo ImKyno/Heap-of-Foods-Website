ba02f6f127effac2cf50aa49a97c6092ac18b56f
Kyno <40477759+ImKyno@users.noreply.github.com>
2026-03-13 20:36:33 -0300

    v2.3-A - Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/heap-of-foods-the-recipe-book",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
